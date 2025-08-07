a1ef588afabc4ac1f335dd3fd60e9b02e1f4db26
DouglasdeCamargo <douglas.camargo.5824@outlook.com>
2025-08-07 19:17:51 -0300

    adicionei o while

---
+++ while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int counter = 1;
+    int numero = 1;
+
+    while (counter <= 10) {
+        printf("test\n");
+        counter++;
+    }
+    return 0;   
+}
